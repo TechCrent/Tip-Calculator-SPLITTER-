489985c64b34971f006dc4540c9790f29ebab598
TechCrent <ciicicrentsil@gmail.com>
2025-12-04 16:28:46 +0000

    Surface level js for project

--- splitter/src/main.js
+++ splitter/src/main.js
@@ -1,4 +1,18 @@
 import './style.css'
 
 //DECLARATION OF SCREEN VARIABLES
-//=>
+//=>Bill input
+const billInput = document.querySelector([data-bill_input]);
+
+//=>Custom input
+const customInput = document.querySelector([data-custom_input]);
+
+//=>Number of People input
+const nopInput = document.querySelector([data-nop_input]);
+
+
+//BUTTONS
+//=>Event listeners for buttons
+const tipButtons = document.querySelectorAll([data-tip]);
+
+//=>Logic behind each button
